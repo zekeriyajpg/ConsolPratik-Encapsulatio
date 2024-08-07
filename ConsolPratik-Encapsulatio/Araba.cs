262a297af2297f4384040bc37becd94201a1ed24
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-07 10:21:01 +0300

    Proje dosyası ekle.

---
+++ ConsolPratik-Encapsulatio/Araba.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolPratik_Encapsulatio
+{
+    public class Araba
+    {
+        // Property to hold the car's brand
+        // Arabanın markasını tutan özellik
+        public string Marka { get; set; }
+        // Property to hold the car's color
+        // Arabanın rengini tutan özellik
+        public int Model { get; set; }
+        // Property to hold the car's color
+        // Arabanın rengini tutan özellik
+        public string Renk { get; set; }
+        // Private field to hold the number of doors
+        // Kapı sayısını tutan özel alan
+        private int _kapısayisi { get; set; }
+        // Public property to get and set the number of doors
+        // Kapı sayısını alıp ayarlamak için genel özellik
+        public int Kapisayisi
+        {
+            get
+            {
+                // Return the number of doors
+                // Kapı sayısını döndür
+                return _kapısayisi;
+            }
+            set
+            {
+                // Check if the value is either 2 or 4
+                // Değerin 2 veya 4 olup olmadığını kontrol et
+                if (value==2 && value==4)
+                {
+                    // Set the number of doors
+                    // Kapı sayısını ayarla
+                    _kapısayisi = value;
+                }
+                else
+                {
+                    // Set the number of doors to -1 for invalid input
+                    // Geçersiz giriş için kapı sayısını -1 olarak ayarla
+                    _kapısayisi = -1;
+                    // Display a warning message
+                    // Uyarı mesajı göster
+                    Console.WriteLine("!!!!!!   uyarı kapı sayısı doğru değil kapı sayınız   !!!!!!!!");
+                }
+            }
+        }
+    }
+}
---
+++ ConsolPratik-Encapsulatio/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsolPratik_Encapsulatio;
+
+Araba araba = new Araba();
+araba.Marka = "BMW";
+araba.Model = 2016;
+araba.Renk = "Mavi";
+araba.Kapisayisi = 3;
+
+
+Console.WriteLine(araba.Marka);
+Console.WriteLine(araba.Model);
+Console.WriteLine(araba.Renk);
+Console.WriteLine(araba.Kapisayisi);
+
+
